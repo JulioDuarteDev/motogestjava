61889f347fcbbdadd5e39cd666b018726172361f
Julio Duarte <julio.franca.017@ufrn.edu.br>
2024-12-01 22:44:26 -0300

    cria controller modelos

---
+++ api/src/main/java/com/motogest/api/layers/controllers/ModelosController.java
@@ -0,0 +1,57 @@
+package com.motogest.api.layers.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.motogest.api.layers.entities.Marcas;
+import com.motogest.api.layers.entities.Modelos;
+import com.motogest.api.layers.services.ModelosService;
+
+@RestController
+@RequestMapping("/modelos")
+public class ModelosController {
+    @Autowired
+    ModelosService modelosService;
+
+    @GetMapping("/all")
+    public List<Modelos> getAllModelos() {
+        return modelosService.listarModelos();
+    }
+
+    @GetMapping("/nome/{nome}")
+    public Modelos getModeloByNome(@PathVariable String nome) {
+        return modelosService.buscarModeloPorNome(nome);
+    }
+
+    @GetMapping("/marca/{idMarca}")
+    public List<Modelos> getModelosByMarca(@PathVariable Integer idMarca) {
+        return modelosService.buscarModelosPorMarca(idMarca);
+    }
+
+    @GetMapping("/cilindrada/{cilindrada}")
+    public List<Modelos> getModelosByCilindrada(@PathVariable Integer cilindrada) {
+        return modelosService.buscarModelosPorCilindrada(cilindrada);
+    }
+
+    @GetMapping("/{id}")
+    public Modelos getModeloById(@PathVariable Integer id) {
+        return modelosService.buscarModeloPorId(id);
+    }
+
+    @PostMapping("/")
+    public String saveModelo(@RequestBody Modelos modelo) {
+        return modelosService.salvarModelo(modelo);
+    }
+
+    @PutMapping("/")
+    public String updateModelo(@RequestBody Modelos modelo) {
+        return modelosService.atualizarModelo(modelo);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteModelo(@PathVariable Integer id) {
+        return modelosService.deletarModelo(id);
+    }
+}
--- api/src/main/java/com/motogest/api/layers/services/ModelosService.java
+++ api/src/main/java/com/motogest/api/layers/services/ModelosService.java
@@ -1,30 +1,101 @@
 package com.motogest.api.layers.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.motogest.api.layers.entities.Marcas;
 import com.motogest.api.layers.entities.Modelos;
 import com.motogest.api.layers.repositories.ModelosRepository;
 
+@Service
 public class ModelosService {
     @Autowired
     private ModelosRepository modelosRepository;
 
+    @Autowired
+    MarcasService marcasService;
+
+    String naoEncontrado = "Não foi possível encontrar um modelo com o parâmetro informado";
+
     public List<Modelos> listarModelos() {
-        return modelosRepository.findAll();
+        List<Modelos> modelos = modelosRepository.findAll();
+
+        if(modelos.isEmpty()) {
+            throw new NoSuchElementException("Não existem modelos cadastrados");
+        }
+        
+        return modelos;
     }
 
-    public Modelos buscarModelo(String nome) {
-        return modelosRepository.findByNome(nome);
+    public Modelos buscarModeloPorNome(String nome) {
+        Modelos modelo = modelosRepository.findByNome(nome);
+
+        if (modelo == null) {
+            throw new NoSuchElementException(naoEncontrado);
+        }
+
+        return modelo;
     }
 
-    public List<Modelos> buscarModelosPorMarca(Marcas marca) {
-        return modelosRepository.findByMarca(marca);
+    public Modelos buscarModeloPorId(Integer id) {
+        return modelosRepository.findById(id).orElseThrow(() -> new NoSuchElementException(naoEncontrado));
+    }
+
+    public List<Modelos> buscarModelosPorMarca(Integer idMarca) {
+        Marcas marca = marcasService.buscarMarcaPorId(idMarca);
+
+        System.out.println(marca);
+        
+        List<Modelos> modelos = modelosRepository.findByMarca(marca);
+
+        if (modelos.isEmpty()) {
+            throw new NoSuchElementException(naoEncontrado);
+        }
+
+        return modelos;
     }
 
     public List<Modelos> buscarModelosPorCilindrada(Integer cilindrada) {
-        return modelosRepository.findByCilindrada(cilindrada);
+        List<Modelos> modelos = modelosRepository.findByCilindrada(cilindrada);
+
+        if (modelos.isEmpty()) {
+            throw new NoSuchElementException(naoEncontrado);
+        }
+
+        return modelos;
+    }
+
+    public String salvarModelo(Modelos modelo) {
+        Modelos modeloExistente = modelosRepository.findByNome(modelo.getNome());
+
+        if(modeloExistente != null) {
+            throw new IllegalArgumentException("Já existe um modelo com o nome informado");
+        }
+
+        modelosRepository.save(modelo);
+
+        String feedback = String.format("Modelo '%s' salvo com sucesso!", modelo.getNome());
+        return feedback;
+    }
+
+    public String atualizarModelo(Modelos modelo) {
+        modelosRepository.findById(modelo.getId()).orElseThrow(() -> new NoSuchElementException(naoEncontrado));
+
+        modelosRepository.save(modelo);
+
+        String feedback = String.format("Modelo '%s' atualizado com sucesso!", modelo.getNome());
+        return feedback;
+    }
+
+    public String deletarModelo(Integer id) {
+        Modelos modelo = modelosRepository.findById(id).orElseThrow(() -> new NoSuchElementException(naoEncontrado));
+
+        modelosRepository.delete(modelo);
+
+        String feedback = String.format("Modelo '%s' deletado com sucesso!", modelo.getNome());
+        return feedback;
     }
 }
